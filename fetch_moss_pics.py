9cc601e2edda4d4ef14b2afde167027e6e0a138e
blop <blopblop913@gmail.com>
2026-01-29 12:23:03 +0000

    Added automatic moss pictures

---
+++ fetch_moss_pics.py
@@ -0,0 +1,13 @@
+import requests
+import os
+
+moss_url = "https://source.unsplash.com/featured/?moss"
+
+def download_moss():
+    response = requests.get(moss_url)
+    if response.status_code == 200:
+        with open("today_moss.jpeg", "wb") as f:
+            f.write(response.content)
+
+if __name__ == "__main__":
+    download_moss()
